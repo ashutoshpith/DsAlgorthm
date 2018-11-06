7da357d1f1071ea728816d6ac1f5c91f29a38d7d
ashutosh <amxdp18@gmail.com>
2018-11-06 17:32:24 +0530

    sum of array with it's only target value

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,35 @@
 public class hello {
+
+        public static int[] twoSum(int[] nums, int target) {
+            for (int i=0; i<nums.length; i++){
+                for (int j=i+1; j<nums.length; j++){
+                    if (nums[j]==target-nums[i]){
+                        return new int[] {i,j};
+                    }
+                }
+            }
+
+throw  new IllegalArgumentException("no two sum");
+        }
+
+
+    public static int fd(int[] a){
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = i+1; j <a.length ; j++) {
+                if (a[i] == a[j]) {
+                    return a[i];
+                }
+            }
+
+        }
+       return -1;
+           }
+
     public static void main(String [] args){
-        System.out.println("Hello Ashutosh");
+            int a[]= {2, 7, 11, 15};
+            int p[]={2, 1, 3, 5, 3, 2};
+            int t = 9;
+       // System.out.println(twoSum(a,t));
+        System.out.println(fd(p));
     }
 }
--- src/swingx.java
+++ src/swingx.java
@@ -35,7 +35,7 @@ public class swingx {
             }
         });
 
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
