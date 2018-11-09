b46c5be12dee4dabb2d458146c4c3a18531b1ae4
ashutosh <amxdp18@gmail.com>
2018-11-09 05:30:06 +0530

    bubble sort ascending and descending order

--- src/Hit.java
+++ src/Hit.java
@@ -6,19 +6,12 @@ public class Hit {
     }
 
     Hit(){
-     int a[] = new int[6];
-        int i=0, t=0;
-
-        Scanner scanner=new Scanner(System.in);
-
-        for ( i = 0; i <a.length ; i++) {
-            a[i] = scanner.nextInt();
+     int a[] = {2,9,5,7,3,6,25};
 
 
-        }
-
-       Bubble_Arrays_sort(a);
-
+      // bubble_arrays_sort_asc(a);
+      //bubble_arrays_sort_desc(a);
+        insertion_sort(a);
 
         for (int j = 0; j <a.length ; j++) {
             System.out.println(a[j]);
@@ -27,18 +20,50 @@ public class Hit {
 
 
     }
-    public  void Bubble_Arrays_sort(int arr[]){
+    public  void bubble_arrays_sort_asc(int arr[]){
         int n = arr.length;
         int temp = 0;
         for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
+            for(int j=0; j < n; j++){
+                if(arr[j] > arr[i]){
                     //swap elements
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
+                    temp = arr[i];
+                    arr[i] = arr[j];
                     arr[j] = temp;
                 }
-                
+
         }
     }
-}}
+}
+    public  void bubble_arrays_sort_desc(int a[]){
+        int n = a.length;
+        int t = 0;
+        for(int i=0; i < n; i++){
+            for(int j=i+1; j < n; j++){
+                if(a[j] > a[i]){
+                    //swap elements
+                    t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+
+            }
+        }
+    }
+
+    public void insertion_sort(int arr[]) {
+        int i,j,t=0;
+        for (i = 0; i <arr.length; i++) {
+            j =i;
+            while ((j>0) && (arr[i] < arr[j-1])){
+                t=arr[i];
+                arr[i]=arr[j];
+                arr[j]=t;
+                j=j-1;
+            }
+
+
+        }
+    }
+
+}
--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -17,6 +17,12 @@ public class InsertionSort {
 
     public static void main(String [] args){
         System.out.println("hello");
+        int a[]={2,9,3,6,4,1};
+        insertionSort(a,5);
+        for (int i = 0; i <a.length ; i++) {
+            System.out.println(a[i]);
+
+        }
 
     }
 }
